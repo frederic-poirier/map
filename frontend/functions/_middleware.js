5b6bfdd7465d56969edece2de94fd68bbbef98aa
frederic-poirier <155044239+frederic-poirier@users.noreply.github.com>
2026-01-21 18:21:33 -0500

    Now using functions for pages

---
+++ frontend/functions/_middleware.js
@@ -0,0 +1,8 @@
+
+export async function onRequest(context) {
+  try {
+    return await context.next();
+  } catch (err) {
+    return new Response(err.message || "Internal Error", { status: 500 });
+  }
+}
--- frontend/worker/src/env.js
+++
@@ -1,7 +0,0 @@
-export function requireEnv(env, keys) {
-	for (const key of keys) {
-		if (!env[key]) {
-			throw new Error(`Missing env var: ${key}`)
-		}
-	}
-}
